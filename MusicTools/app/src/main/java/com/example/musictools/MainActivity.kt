9d823ea0509c322942bc31aba8e3dd8b7bcedbba
황 준호 <F2020009@fescaro.database>
2021-03-09 15:55:24 +0900

    Add LoginPage

---
+++ MusicTools/app/src/main/java/com/example/musictools/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musictools
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
+    }
+}
